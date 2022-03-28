d5ab07dc4e11d2e9d46e5e5acb8023290fdda329
clafaurieg <70864691+clafaurieg@users.noreply.github.com>
2022-03-27 23:41:30 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,5 @@
+var formEl = document.querySelector('form')
+var inputEl = document.querySelector('input')
+var weatherEl = document.getElementById('weather')
+console.log (formEl, inputEl, weatherEl)
+
